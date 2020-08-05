688b8584e7cfd0441407510aa5bcfc5ec6e991ae
Joshua Wilensky <joshwilensky@gmail.com>
2020-08-05 00:04:13 -0400

    Finished seedDB.sql

--- seedDB.sql
+++ seedDB.sql
@@ -6,9 +6,9 @@ CREATE TABLE departments
 (
     id INTEGER
     AUTO_INCREMENT NOT NULL,
-    department VARCHAR
+department VARCHAR
     (30),
-    PRIMARY KEY
+PRIMARY KEY
     (id)
 );
 
@@ -16,17 +16,65 @@ CREATE TABLE departments
     (
         id INTEGER
         AUTO_INCREMENT NOT NULL,
-        title VARCHAR
+title VARCHAR
         (30),
-        salary DECIMAL),
-        department_id INTEGER,
-
-        PRIMARY KEY
+salary DECIMAL,
+department_id INTEGER,
+PRIMARY KEY
         (id),
-
-        FOREIGN KEY
+FOREIGN KEY
         (department_id) REFERENCES department
-        (id),
-        );
+        (id)
+);
 
         CREATE TABLE employees
+        (
+            id INTEGER
+            AUTO_INCREMENT NOT NULL, 
+first_name VARCHAR
+            (30) NOT NULL,
+last_name VARCHAR
+            (30) NOT NULL,
+role_id INTEGER,
+manager_id INTEGER,
+PRIMARY KEY
+            (id),
+FOREIGN KEY
+            (role_id) REFERENCES roles
+            (id),
+FOREIGN KEY
+            (manager_id) REFERENCES employees
+            (id)
+);
+
+            INSERT INTO departments
+                (department)
+            VALUES
+                ("Initech");
+
+            INSERT INTO roles
+                (title, salary, department_id)
+            VALUES
+                ("Manager", "150000", 1),
+                ("Salary", "25000", 1);
+
+            INSERT INTO employees
+                (first_name, last_name, role_id, manager_id)
+            VALUES
+                ("Bill", "Lumbergh ", 2, 1);
+            INSERT INTO employees
+                (first_name, last_name, role_id, manager_id)
+            VALUES
+                ("Peter", "Gibbons", 1);
+            INSERT INTO employees
+                (first_name, last_name, role_id, manager_id)
+            VALUES
+                ("Milton", "Waddams", 2, 1);
+            INSERT INTO employees
+                (first_name, last_name, role_id, manager_id)
+            VALUES
+                ("Samir", "Nagheenanajar ", 2, 1);
+            INSERT INTO employees
+                (first_name, last_name, role_id, manager_id)
+            VALUES
+                ("Michael", "Bolton", 2, 1);
